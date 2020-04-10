2deb762164f48dfc85beeb89b2c78c19afe70668
Caleb Abbott <54942782+Cabbott118@users.noreply.github.com>
2020-04-10 16:30:42 -0400

    Code organized into appropriate folders.

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: 'AIzaSyBam5Xw71HzPcwqbUjFGYJF63we2wDQCWM',
+  authDomain: 'social-media-cha.firebaseapp.com',
+  databaseURL: 'https://social-media-cha.firebaseio.com',
+  projectId: 'social-media-cha',
+  storageBucket: 'social-media-cha.appspot.com',
+  messagingSenderId: '789984777785',
+  appId: '1:789984777785:web:fdba84a7060e31d0b1dfe8',
+  measurementId: 'G-2SDQQ7MS2R',
+};
