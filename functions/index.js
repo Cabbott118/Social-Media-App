898a9e9988bc877ca7aac41100b99cbff4a6e43f
Caleb Abbott <54942782+Cabbott118@users.noreply.github.com>
2020-04-12 08:49:11 -0400

    Get route for singular post, found by id.

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ const functions = require('firebase-functions');
 const app = require('express')();
 const FBAuth = require('./util/FBAuth');
 
-const { getAllPosts, postOnePost } = require('./handlers/posts');
+const { getAllPosts, postOnePost, getPost } = require('./handlers/posts');
 const {
   signUp,
   login,
@@ -11,15 +11,10 @@ const {
   addUserDetails,
 } = require('./handlers/users');
 
-// const firebase = require('firebase/app');
-// require('./node_modules/firebase/firebase-app.js');
-// require('./node_modules/firebase/firebase-auth');
-
-// firebase.initializeApp(firebaseConfig);
-
 // Posts Routes
 app.get('/posts', getAllPosts);
 app.post('/post', FBAuth, postOnePost);
+app.get('/post/:postId', getPost);
 
 // User Routes
 app.post('/signup', signUp);
