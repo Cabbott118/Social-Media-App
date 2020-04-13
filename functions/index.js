beeb0b1723cd42c6f92482121d63b484e59d846d
Caleb Abbott <54942782+Cabbott118@users.noreply.github.com>
2020-04-13 11:54:07 -0400

    Functions for updating when user deletes post or changes profile image.

--- functions/index.js
+++ functions/index.js
@@ -98,3 +98,53 @@ exports.createNotificationOnComment = functions.firestore
         return;
       });
   });
+
+exports.onUserImageChange = functions.firestore
+  .document('/users/{userId}')
+  .onUpdate((change) => {
+    if (change.before.data().imageUrl !== change.after.data().imageUrl) {
+      const batch = db.batch();
+      return db
+        .collection('posts')
+        .where('userHandle', '==', change.before.data().handle)
+        .get()
+        .then((data) => {
+          data.forEach((doc) => {
+            const post = db.doc(`/posts/${doc.id}`);
+            batch.update(post, { userImage: change.after.data().imageUrl });
+          });
+          return batch.commit();
+        });
+    }
+  });
+exports.onPostDelete = functions.firestore
+  .document('/posts/{postId}')
+  .onDelete((snapshot, context) => {
+    const postId = context.params.postId;
+    const batch = db.batch();
+    return db
+      .collection('comments')
+      .where('postId', '==', postId)
+      .get()
+      .then((data) => {
+        data.forEach((doc) => {
+          batch.delete(db.doc(`/comments/${doc.id}`));
+        });
+        return db.collection('likes').where('postId', '==', postId);
+      })
+      .then((data) => {
+        data.forEach((doc) => {
+          batch.delete(db.doc(`/likes/${doc.id}`));
+        });
+        return db.collection('notifications').where('postId', '==', postId);
+      })
+      .then((data) => {
+        data.forEach((doc) => {
+          batch.delete(db.doc(`/notifications/${doc.id}`));
+        });
+        return batch.commit();
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  });
